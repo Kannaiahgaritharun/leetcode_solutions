e8ffeda9ffd9724b6d0b2b7411fd594e1175b25d
Kannaiahgari Tharun <kannaiahgaritharun@gmail.com>
2026-08-06 13:29:00 +0530

    Time: 48 ms (10.27%), Space: 37.3 MB (9.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,23 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        for key in freq:
+            if freq[key] > 1:
+                return True
+          
+        return False
+    
+
+   
+
+
+
+
+
+
+        
